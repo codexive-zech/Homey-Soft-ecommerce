72d229a9e43ec273b23f17607bf207d02f6de46f
Codexive_zech <zechariahhounwanou@gmail.com>
2022-10-03 01:34:24 +0100

    Added Navbar, Footer, Sidebar and cart buttons

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import About from "../pages/AboutPage";
+import Cart from "../pages/CartPage";
+import Checkout from "../pages/CheckoutPage";
+import Error from "../pages/ErrorPage";
+import Products from "../pages/ProductsPage";
+import SingleProduct from "../pages/SingleProductPage";
+import PrivateRoute from "../pages/PrivateRoute";
+import Home from "../pages/HomePage";
+
+export {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  Products,
+  PrivateRoute,
+  SingleProduct,
+  Home,
+};
