024beb1ae04e2b1d3613cbfc0a5fdc2e71aaee5f
Codexive_zech <zechariahhounwanou@gmail.com>
2022-10-03 04:05:49 +0100

    Added Error Page, About Page, and Home Page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice comfy" />
+        <article>
+          <div className="title">
+            <h2>Our Story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+            magnam dolore ad minima ducimus unde soluta ut veniam, quam
+            molestiae vel, magni optio suscipit ipsa pariatur blanditiis et rem
+            vero libero nemo similique. Repellendus dolorem quae explicabo
+            libero voluptatibus. Minus, dolor placeat corporis facere voluptatem
+            animi fugiat. Eaque, inventore magni.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +54,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
