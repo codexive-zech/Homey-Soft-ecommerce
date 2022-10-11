a72b9a3edb0cdd857c968d458072e0e5a278e43a
Codexive_zech <zechariahhounwanou@gmail.com>
2022-10-11 03:18:15 +0100

    Implemented Single product stars (Array approach)

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -2,61 +2,23 @@ import React from "react";
 import styled from "styled-components";
 import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
 const Stars = ({ stars, reviews }) => {
-  console.log(stars, reviews);
+  const tempStars = Array.from({ length: 5 }, (_, index) => {
+    const getHalfNumb = index + 0.5;
+    return (
+      <span>
+        {stars >= index + 1 ? (
+          <BsStarFill />
+        ) : stars >= getHalfNumb ? (
+          <BsStarHalf />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+    );
+  });
   return (
     <Wrapper>
-      <div className="stars">
-        {/* 1 Star */}
-        <span>
-          {stars >= 1 ? (
-            <BsStarFill />
-          ) : stars >= 0.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* 2 Star */}
-        <span>
-          {stars >= 2 ? (
-            <BsStarFill />
-          ) : stars >= 2.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* 3 Star */}
-        <span>
-          {stars >= 3 ? (
-            <BsStarFill />
-          ) : stars >= 2.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* 1 Star */}
-        <span>
-          {stars >= 4 ? (
-            <BsStarFill />
-          ) : stars >= 3.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* 1 Star */}
-        <span>
-          {stars >= 5 ? (
-            <BsStarFill />
-          ) : stars >= 4.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-      </div>
+      <div className="stars">{tempStars}</div>
       <p className="reviews">({reviews} Customer Reviews)</p>
     </Wrapper>
   );
